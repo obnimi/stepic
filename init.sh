ee6cdd2f6fd61525573b02204cc19d6d73bd781a
obnimi <sterroidxd@mail.ru>
2016-03-09 12:56:02 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo /etc/init.d/mysql restart
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
