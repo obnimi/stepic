b754864bd35694dc7a644ef82561e170b378cd91
obnimi <sterroidxd@mail.ru>
2016-03-09 12:51:53 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,46 +1,6 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse, Http404
-from django.core.paginator import Paginator
-from django.views.decorators.http import require_GET
-from models import Question
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
-
-@require_GET
-def index(request):
-    try:
-        page = int(request.GET.get('page', 1))
-    except ValueError:
-        raise Http404
-    questions = Question.objects.order('-added_at')
-    paginator = Paginator(questions, 10)
-    paginator.baseURL = "/?page="
-    page = paginator.page(page)
-    return render_to_response('questions_list.html', {
-        'questions': page.object_list,
-        'paginator': paginator,
-        'page': page,
-    })
-
-@require_GET
-def popular(request):
-    try:
-        page = int(request.GET.get('page', 1))
-    except ValueError:
-        raise Http404
-    questions = Question.objects.order('-rating')
-    paginator = Paginator(questions, 10)
-    paginator.baseURL = "/popular/?page="
-    page = paginator.page(page)
-    return render_to_response('questions_list.html', {
-        'questions': page.object_list,
-        'paginator': paginator,
-        'page': page,
-    })
-
-@require_GET
-def question(request, id):
-    question = get_object_or_404(Question, id=id)
-    return render_to_response("question_detail.html", {"question": question})
