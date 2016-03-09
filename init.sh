04cf57a27051673d04fc82328cd0f4c4322263d7
obnimi <sterroidxd@mail.ru>
2016-03-09 11:42:08 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
