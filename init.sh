cc02b4465e66c506c6230d1e7ada8d471bd94797
obnimi <sterroidxd@mail.ru>
2016-03-09 11:25:50 +0300

    Обновлено init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+cd
+mkdir etc
+sudo mv /home/box/web/etc/gunicorn.conf /home/box/etc/
+sudo ln -sf /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:app
