d31c6ec54de4db68a101581508e1c5f3584b6955
obnimi <sterroidxd@mail.ru>
2016-03-09 12:54:01 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,4 @@ sudo /etc/init.d/mysql restart
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_django.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
-mysql -u root < /home/box/web/create.sql
