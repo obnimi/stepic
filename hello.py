59285d906de23694db81ecbab6c499d5202ba4af
Max <sterroidxd@mail.ru>
2016-03-09 11:17:14 +0300

    add hello.py

--- hello.py
+++ hello.py
@@ -1,47 +1,8 @@
-from __future__ import unicode_literals
+bind="0.0.0.0:8080"
 
-import multiprocessing
-
-import gunicorn.app.base
-
-from gunicorn.six import iteritems
-
-
-def number_of_workers():
-    return (multiprocessing.cpu_count() * 2) + 1
-
-
-def handler_app(environ, start_response):
-	query = environ['QUERY_STRING'].split("&")
-	data = "\n".join(query)
-	status = "200 OK"
-	response_headers = [
-			('Content-Type', 'text/plain'),
-			('Content-Length', str(len(data)))
-		]
-	start_response(status, response_headers)
-	return [bytes(data)]
-
-class StandaloneApplication(gunicorn.app.base.BaseApplication):
-
-    def __init__(self, app, options=None):
-        self.options = options or {}
-        self.application = app
-        super(StandaloneApplication, self).__init__()
-
-    def load_config(self):
-        config = dict([(key, value) for key, value in iteritems(self.options)
-                       if key in self.cfg.settings and value is not None])
-        for key, value in iteritems(config):
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
-
-
-if __name__ == '__main__':
-    options = {
-        'bind': '%s:%s' % ('0.0.0.0', '8080'),
-        'workers': number_of_workers(),
-    }
-    StandaloneApplication(handler_app, options).run()
+def app (environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type','text/plain')]
+    start_response(status, response_headers)
+    options = '\r\n'.join(environ['QUERY_STRING'].split("&"))
+    return [options]
